50797569e5886cb3d119604f6e57f0d23df102cb
Rachel Church <rachel.church@clover.com>
2020-09-22 12:08:57 -0700

    Added filter input, apollo caching config, and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { DogToy, useGetToysUnderPriceQuery } from './schema';
+import { API_KEY, CONTENTFUL_GRAPHQL_ENDPOINT } from 'apolloClient';
+import { DogToy, useGetToysUnderPriceQuery } from 'schema';
 
 import './App.css';
 
-// Utilize the environment variables defined in the `.env` file
-const {
-  REACT_APP_CONTENTFUL_SPACE_ID: SPACE_ID,
-  REACT_APP_CONTENTFUL_ENVIRONMENT: ENVIRONMENT,
-  REACT_APP_CONTENTFUL_API_KEY: API_KEY,
-} = process.env;
-
-const CONTENTFUL_GRAPHQL_ENDPOINT = `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/environments/${ENVIRONMENT}`;
-
 const App = () => {
+  const [maxPrice, setMaxPrice] = React.useState<number>(200);
   const { loading, error, data } = useGetToysUnderPriceQuery({
-    variables: {
-      maxPrice: 20
-    }
+    variables: { maxPrice: maxPrice || Number.POSITIVE_INFINITY }
   });
 
   // Use optional chaining to retrieve the nested items array OR default to an empty array
@@ -25,27 +16,32 @@ const App = () => {
   if (error) {
     console.error(error);
     return <div className="app"><h1>Oops! Something went wrong</h1></div>
-  } else if (loading) {
-    return <div className="app">
-      <div className="loader"/>
-    </div>;
   }
 
   return (
     <div className="app">
       <div className="container">
         <h1>Welcome! These are Cowboy's toys</h1>
-        <ul className="productList">
-          {allToys.map((toy) =>
-            <li key={toy.sys.id} id={toy.sys.id} className="productCard">
-              <h2>
-                <span>{toy.toyName}</span>
-                <span>${toy.estimatedPrice}</span>
-              </h2>
-              {toy.toyPhoto?.url && <img src={toy.toyPhoto.url} alt=""/>}
-            </li>,
-          )}
-        </ul>
+        <label className="priceFilter">
+          <span>Filter toys by price</span>
+          <div className="dollarInputWrapper">
+            <span className="dollarSign">$</span>
+            <input value={maxPrice} type="number" min="0" max="1000" step="1" onChange={(e) => setMaxPrice(Number(e.target.value))} />
+          </div>
+        </label>
+        {
+          loading ? <div className="loader"/> : (<ul className="productList">
+            {allToys.map((toy) =>
+              <li key={toy.sys.id} id={toy.sys.id} className="productCard">
+                <h2>
+                  <span>{toy.toyName}</span>
+                  <span>${toy.estimatedPrice}</span>
+                </h2>
+                {toy.toyPhoto?.url && <img src={toy.toyPhoto.url} alt=""/>}
+              </li>,
+            )}
+          </ul>)
+        }
       </div>
       <footer>
         <div className="container">
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from 'apolloClient';
 
-// Utilize the environment variables defined in the `.env` file
-const {
-  REACT_APP_CONTENTFUL_SPACE_ID: SPACE_ID,
-  REACT_APP_CONTENTFUL_ENVIRONMENT: ENVIRONMENT,
-  REACT_APP_CONTENTFUL_API_KEY: API_KEY,
-} = process.env;
-
-export const apolloClient = new ApolloClient({
-  uri: `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/environments/${ENVIRONMENT}`,
-  headers: {
-    Authorization: `Bearer ${API_KEY}`,
-  },
-  cache: new InMemoryCache(),
-});
+import App from 'App';
+import 'index.css';
 
 ReactDOM.render(
   <React.StrictMode>
